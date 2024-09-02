13147938e70e606000af066ed3414e3a1c3e961f
Surya Narayan Sahu <suryanarayansahu10@gmail.com>
2024-09-03 00:00:44 +0530

    test1

---
+++ demo/src/main/java/com/example/demo/controller/XyzControoler.java
@@ -0,0 +1,9 @@
+package com.example.demo.controller;
+
+
+public class XyzControoler {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    //System.out.println("hello");
+}
