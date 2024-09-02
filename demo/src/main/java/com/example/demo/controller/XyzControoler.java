741c981009fb5bf6de41d5660232776548566c3b
Surya Narayan Sahu <161349759+suryanarayansahu10@users.noreply.github.com>
2024-09-03 00:11:33 +0530

    Update XyzControoler.java

--- demo/src/main/java/com/example/demo/controller/XyzControoler.java
+++ demo/src/main/java/com/example/demo/controller/XyzControoler.java
@@ -4,6 +4,7 @@ package com.example.demo.controller;
 public class XyzControoler {
     public static void main(String[] args) {
         System.out.println("hello1111111111111");
+        System.out.println("second change");
     }
     //System.out.println("hello");
 }
